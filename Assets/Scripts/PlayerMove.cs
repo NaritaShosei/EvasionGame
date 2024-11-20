6562543b4caa97bdb20cecd8f4e052cd609dd2e0
NaritaShosei <onigirikun7171@gmail.com>
2024-11-20 17:31:03 +0900

    弾を放つコンポーネント

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D _rb;
     [SerializeField] float _moveSpeed = 5f;
+    [NonSerialized] public Vector2 mousePos;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -14,8 +16,8 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.localRotation = Quaternion.LookRotation(transform.forward,mousePos);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.localRotation = Quaternion.LookRotation(transform.forward, mousePos - (Vector2)transform.position);
     }
 
     void FixedUpdate()
---
+++ Assets/Scripts/PlayerShooter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    [SerializeField] float _bulletSpeed;
+    [SerializeField] Transform _muzzlePos;
+    [SerializeField] List<GameObject> _bulletPrefab;//í èÌíeÇ∆ì¡éÍíeÇ…Ç∑ÇÈÇ©Ç‡
+    PlayerMove _player;
+
+    private void Start()
+    {
+        _player = GetComponent<PlayerMove>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var bullet = Instantiate(_bulletPrefab[0], _muzzlePos.position, Quaternion.LookRotation(transform.forward, _player.mousePos));
+            var bulletRb = bullet.GetComponent<Rigidbody2D>();
+            var direction = (_player.mousePos - (Vector2)_muzzlePos.position).normalized;
+            bulletRb.velocity = _bulletSpeed * direction;
+            Destroy(bullet,2f);
+        }
+    }
+}
