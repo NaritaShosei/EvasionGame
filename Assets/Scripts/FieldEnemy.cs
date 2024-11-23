c99442ca4657dad17147b486f730bb045308f67e
NaritaShosei <onigirikun7171@gmail.com>
2024-11-23 20:43:32 +0900

    安置をなくすenemy

---
+++ Assets/Scripts/FieldEnemy.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class FieldEnemy : MonoBehaviour
+{
+    [SerializeField] float _interval = 5;
+    float _timer;
+    [SerializeField] SpriteRenderer _sr;
+    [SerializeField] CircleCollider2D _collider;
+    PlayerController _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = FindObjectOfType<PlayerController>();
+        _timer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > _timer + _interval)
+        {
+            _timer = Time.time;
+            Attack();
+        }
+
+    }
+
+    private void Attack()
+    {
+        transform.position = _player.transform.position;
+        _sr.DOFade(1, 1).OnComplete(() =>
+        {
+            _sr.color = Color.red;
+            _collider.enabled = true;
+            StartCoroutine(Invisible());
+        });
+
+    }
+
+    IEnumerator Invisible()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Attack8second");
+        _collider.enabled = false;
+        _sr.DOFade(0, 0.5f).OnComplete(() => _sr.color = new Color(1, 1, 1, 0));
+        _timer = Time.time;
+    }
+}
